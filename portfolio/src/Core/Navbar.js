b03caf5725b7bc69fb94e936417cbb0ca1f16ff7
Jesus Esquer <jme2791@gmail.com>
2020-06-02 19:28:01 -0700

    Created Navbar component with spinner

---
+++ portfolio/src/Core/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="navbar fixed-top navbar-light bg-light">
+    <a
+      className="navbar-brand"
+      href="https://fontawesome.com/icons/"
+      target="_blank"
+    >
+      <i id="spin" className="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+    </a>
+  </nav>
+);
+
+export default Navbar;

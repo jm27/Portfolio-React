29f7c1b62872e81ff1289fc908dda8fd20ae831e
Jesus Esquer <jme2791@gmail.com>
2020-06-04 19:48:36 -0700

    added grid to intro

--- portfolio/src/Core/Section.js
+++ portfolio/src/Core/Section.js
@@ -4,7 +4,6 @@ const Section = ({ title = "intro" }) => {
   return (
     <div>
       <section className={title}>
-        <div className='container-fluid'></div>
       </section>
     </div>
   );
--- portfolio/src/Sections/Intro.js
+++ portfolio/src/Sections/Intro.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Section from '../Core/Section'
+import React from "react";
+import Section from "../Core/Section";
 
 const Intro = () => {
+  return (
+      <section className='intro'>
+    <div className="container-fluid container-intro text-center">
+      <h2>Jesus Esquer</h2>
+      <hr></hr>
+      <h3>Full Stack Developer</h3>
+    </div>
+    </section>
+  );
+};
 
-    return (
-        <Section>
-            
-        </Section>
-    )
-}
-
-export default Intro
+export default Intro;

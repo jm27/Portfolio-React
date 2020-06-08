65a99484c756482e91e362fa1c801ce98a13b53a
Jesus Esquer <jme2791@gmail.com>
2020-06-08 13:23:26 -0700

    Created Card component, added e-commerce card

--- portfolio/src/Sections/Portfolio.js
+++ portfolio/src/Sections/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../Core/Card";
+
 const Portfolio = () => (
   <section className="portfolio justify-content-center text-center">
     <div
@@ -8,14 +9,54 @@ const Portfolio = () => (
     >
       <h2 id="title">My recent work</h2>
       <div className="projects">
-        <Card
-          cardImg="https://www.cloudways.com/blog/wp-content/uploads/Ecommerce-Security-Tips-1280x720.jpg"
-          cardTitle="E-commerce"
-          cardText="
-          MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-          deployedLink="https://ecomlearnshop.com/"
-          repoLink="https://github.com/jm27/E-Commerce-MERN"
-        ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
+            <Card
+            cardImg={require("../media/e-commerce.jpg")}
+            cardTitle="E-commerce"
+            cardText="
+            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+            deployedLink="https://ecomlearnshop.com/"
+            repoLink="https://github.com/jm27/E-Commerce-MERN"
+            ></Card>
       </div>
     </div>
   </section>
--- portfolio/src/Sections/Profile.js
+++ portfolio/src/Sections/Profile.js
@@ -3,8 +3,7 @@ import React from "react";
 const Profile = () => (
   <section className="profile justify-content-center text-center">
     <div
-      className="container container-profile
-        "
+      className="container container-profile"
     >
       <h1 className="mt-4 profile-title">Hello World!</h1>
       <hr className="border-white" />

b9703881665ce20b28dd48b826b96aa8fb70da8a
Jesus Esquer <jme2791@gmail.com>
2020-06-08 10:09:45 -0700

    Created Card component, added e-commerce card

--- portfolio/src/Sections/Portfolio.js
+++ portfolio/src/Sections/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Card from "../Core/Card";
 const Portfolio = () => (
   <section className="portfolio justify-content-center text-center">
     <div
@@ -7,7 +7,16 @@ const Portfolio = () => (
         mb-4"
     >
       <h2 id="title">My recent work</h2>
-      <div className="projects"></div>
+      <div className="projects">
+        <Card
+          cardImg="https://www.cloudways.com/blog/wp-content/uploads/Ecommerce-Security-Tips-1280x720.jpg"
+          cardTitle="E-commerce"
+          cardText="
+          MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
+          deployedLink="https://ecomlearnshop.com/"
+          repoLink="https://github.com/jm27/E-Commerce-MERN"
+        ></Card>
+      </div>
     </div>
   </section>
 );

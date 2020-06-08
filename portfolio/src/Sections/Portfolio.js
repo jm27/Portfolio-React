3cc3ae27a71bd497662bc4382ffb5751d8980f56
Jesus Esquer <jme2791@gmail.com>
2020-06-08 13:35:27 -0700

    Created Card component, added react etch-a-sketch card

--- portfolio/src/Sections/Portfolio.js
+++ portfolio/src/Sections/Portfolio.js
@@ -20,24 +20,24 @@ const Portfolio = () => (
         <Card
           cardImg={require("../media/crystal-React.jpg")}
           cardTitle="Crystal React Game"
-          cardText="Crystal game where you have to guess the number that each crystal represent. Built with React and CSS"
+          cardText="Crystal game where you have to guess the number that each crystal represent. Built with React.js and CSS"
           deployedLink="https://jm27.github.io/React-Crystals/"
           repoLink="https://github.com/jm27/React-Crystals"
         ></Card>
         <Card
           cardImg={require("../media/css-grid-2.jpg")}
-          cardTitle="E-commerce"
-          cardText="Playground for Css Grid, An application with 10 different CSS Grid examples."
+          cardTitle="CSS Grid Playground"
+          cardText="Playground for Css Grid, An application with 10 different CSS Grid examples. Built with HTML and CSS"
           deployedLink="https://jm27.github.io/Css-Grid-pg/"
           repoLink="https://github.com/jm27/Css-Grid-pg"
         ></Card>
         <Card
-          cardImg={require("../media/e-commerce.jpg")}
-          cardTitle="E-commerce"
-          cardText="
-            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-          deployedLink="https://ecomlearnshop.com/"
-          repoLink="https://github.com/jm27/E-Commerce-MERN"
+          cardImg={require("../media/etch-a-sketch.jpg")}
+          cardTitle="Etch-A-Sketch"
+          cardText="This game is going to take you back to your childhood. Built with JavaScript, CSS and HTML.
+          "
+          deployedLink="https://jm27.github.io/Etch-A-Sketch/"
+          repoLink="https://github.com/jm27/Etch-A-Sketch"
         ></Card>
         <Card
           cardImg={require("../media/e-commerce.jpg")}

ceb69c48705de84e9d3b5ebc537a099c124d3cc7
Jesus Esquer <jme2791@gmail.com>
2020-06-08 13:30:12 -0700

    Created Card component, added react crystal, css-grid card

--- portfolio/src/Sections/Portfolio.js
+++ portfolio/src/Sections/Portfolio.js
@@ -9,54 +9,52 @@ const Portfolio = () => (
     >
       <h2 id="title">My recent work</h2>
       <div className="projects">
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
+        <Card
+          cardImg={require("../media/e-commerce.jpg")}
+          cardTitle="E-commerce"
+          cardText="
             MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
+          deployedLink="https://ecomlearnshop.com/"
+          repoLink="https://github.com/jm27/E-Commerce-MERN"
+        ></Card>
+        <Card
+          cardImg={require("../media/crystal-React.jpg")}
+          cardTitle="Crystal React Game"
+          cardText="Crystal game where you have to guess the number that each crystal represent. Built with React and CSS"
+          deployedLink="https://jm27.github.io/React-Crystals/"
+          repoLink="https://github.com/jm27/React-Crystals"
+        ></Card>
+        <Card
+          cardImg={require("../media/css-grid-2.jpg")}
+          cardTitle="E-commerce"
+          cardText="Playground for Css Grid, An application with 10 different CSS Grid examples."
+          deployedLink="https://jm27.github.io/Css-Grid-pg/"
+          repoLink="https://github.com/jm27/Css-Grid-pg"
+        ></Card>
+        <Card
+          cardImg={require("../media/e-commerce.jpg")}
+          cardTitle="E-commerce"
+          cardText="
             MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
+          deployedLink="https://ecomlearnshop.com/"
+          repoLink="https://github.com/jm27/E-Commerce-MERN"
+        ></Card>
+        <Card
+          cardImg={require("../media/e-commerce.jpg")}
+          cardTitle="E-commerce"
+          cardText="
             MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
+          deployedLink="https://ecomlearnshop.com/"
+          repoLink="https://github.com/jm27/E-Commerce-MERN"
+        ></Card>
+        <Card
+          cardImg={require("../media/e-commerce.jpg")}
+          cardTitle="E-commerce"
+          cardText="
             MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
-            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
-            <Card
-            cardImg={require("../media/e-commerce.jpg")}
-            cardTitle="E-commerce"
-            cardText="
-            MERN Stack React Node MongoDB Express powered E-Commerce App with PayPal and Credit Card Payment along with Admin Dashboard!"
-            deployedLink="https://ecomlearnshop.com/"
-            repoLink="https://github.com/jm27/E-Commerce-MERN"
-            ></Card>
+          deployedLink="https://ecomlearnshop.com/"
+          repoLink="https://github.com/jm27/E-Commerce-MERN"
+        ></Card>
       </div>
     </div>
   </section>

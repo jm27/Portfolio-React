5f8f239f1f65b873f7aaeb0d047b93ebe7d58527
Jesus Esquer <jme2791@gmail.com>
2020-06-10 15:38:30 -0700

    Updates

---
+++ precache-manifest.8c84d88e025bde08a5123b9acd3cfe0a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b93e2afdb4b564c0832b33203ef302",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "4680ed9b4dd85c1785b7",
+    "url": "/Portfolio-React/static/css/main.99443499.chunk.css"
+  },
+  {
+    "revision": "2b7998fa54634bdaf183",
+    "url": "/Portfolio-React/static/js/2.c375a978.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/Portfolio-React/static/js/2.c375a978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4680ed9b4dd85c1785b7",
+    "url": "/Portfolio-React/static/js/main.ec45f7db.chunk.js"
+  },
+  {
+    "revision": "e5c0292583e9a5a5c925",
+    "url": "/Portfolio-React/static/js/runtime-main.b3550668.js"
+  },
+  {
+    "revision": "e675cd5bf641fa4bf8a478b24ffd9e39",
+    "url": "/Portfolio-React/static/media/burger.e675cd5b.png"
+  },
+  {
+    "revision": "698a3b22bcc9e1727e68436993b88ac6",
+    "url": "/Portfolio-React/static/media/clicky-game.698a3b22.png"
+  },
+  {
+    "revision": "4f3fae869012e6839f3d4f003e266f5c",
+    "url": "/Portfolio-React/static/media/css-grid-2.4f3fae86.jpg"
+  },
+  {
+    "revision": "8fd0faf96e5975fbfcf23184e3a12657",
+    "url": "/Portfolio-React/static/media/e-commerce.8fd0faf9.jpg"
+  },
+  {
+    "revision": "6d499d6897b444a06fffbe1fc2ee072d",
+    "url": "/Portfolio-React/static/media/etch-a-sketch.6d499d68.jpg"
+  },
+  {
+    "revision": "ae74261d1c1175a278a0d6ce4034ca4e",
+    "url": "/Portfolio-React/static/media/hero.ae74261d.svg"
+  },
+  {
+    "revision": "8d039d9c410e23ba0b6a7498e7620618",
+    "url": "/Portfolio-React/static/media/pp-2.8d039d9c.png"
+  }
+]);
